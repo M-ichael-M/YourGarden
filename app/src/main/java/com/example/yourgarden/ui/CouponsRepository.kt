2399e5b9d8557d73e8635e2495c16b95f2cccd45
Michał Małeczek <michalmaleczek@gmail.com>
2025-03-22 23:13:55 +0100

    Optimizes and fixes

--- app/src/main/java/com/example/yourgarden/ui/CouponsRepository.kt
+++ app/src/main/java/com/example/yourgarden/ui/CouponsRepository.kt
@@ -20,6 +20,13 @@ class CouponsRepository(private val couponsDao: CouponsDao) {
     suspend fun insertInitialCoupons() {
         val initialCoupons = listOf(
             CouponsEntity(
+                title = "BEST BIRTHDAY OF YOUR LIFE ",
+                code = "28062025",
+                description = "Dostępne tylko 28.03.2025!",
+                used = false,
+                date = null
+            ),
+            CouponsEntity(
                 title = "SWEET SURPRISE ",
                 code = "1",
                 description = "Aktywować najpóźniej 24h przed randką!",
@@ -28,35 +35,35 @@ class CouponsRepository(private val couponsDao: CouponsDao) {
             ),
             CouponsEntity(
                 title = "DINNER SURPRISE (outside)",
-                code = "13",
+                code = "2",
                 description = "Aktywować najpóźniej 24h przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "DINNER SURPRISE (at home)",
-                code = "7",
+                code = "3",
                 description = "Aktywować najpóźniej 24h przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "FILM SURPRISE",
-                code = "2",
+                code = "4",
                 description = "",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "DATE SURPRISE (outside)",
-                code = "2137",
+                code = "5",
                 description = "Aktywować najpóźniej 48h przed randką, dostępne jak jest ciepło!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "DATE SURPRISE (at home)",
-                code = "7312",
+                code = "6",
                 description = "Aktywować najpóźniej 48h przed randką!",
                 used = false,
                 date = null
@@ -70,42 +77,42 @@ class CouponsRepository(private val couponsDao: CouponsDao) {
             ),
             CouponsEntity(
                 title = "FREAKY SURPRISE",
-                code = "999",
+                code = "7",
                 description = "Aktywować najpóźniej 2 tygodnie przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "DIY SURPRISE (from me)",
-                code = "81",
+                code = "8",
                 description = "Aktywować najpóźniej 48h przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "DIY SURPRISE (we make it)",
-                code = "777",
+                code = "9",
                 description = "Aktywować najpóźniej 48h przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "SPA SURPRISE",
-                code = "44",
+                code = "10",
                 description = "Aktywować najpóźniej 48h przed randką!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "BEAUTY SPOT SURPRISE",
-                code = "32",
+                code = "11",
                 description = "Aktywować tylko w ładną pogodę!",
                 used = false,
                 date = null
             ),
             CouponsEntity(
                 title = "COFFEHOUSE TEST",
-                code = "10",
+                code = "2115",
                 description = "Aktywować najpóźniej 24h przed randką!",
                 used = false,
                 date = null
@@ -119,7 +126,7 @@ class CouponsRepository(private val couponsDao: CouponsDao) {
             ),
             CouponsEntity(
                 title = "COSY SURPRISE",
-                code = "987",
+                code = "2137",
                 description = "Aktywować w dowolnym momencie!",
                 used = false,
                 date = null
